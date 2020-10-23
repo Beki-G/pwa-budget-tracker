c8c5cc95624d9026c355bf85ad849e8343d5fa64
Izuku <63758052+Beki-G@users.noreply.github.com>
2020-10-23 16:35:29 -0700

    add route for html

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost/budget", {useUni
 
 // routes
 app.use(require("./routes/api.js"));
+require("./routes/htmlRoutes")(app)
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
